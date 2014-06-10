c77587ceca1741e9b9822384580cb2d37fa37108
Daniel van den Berg <idrini@gmail.com>
2014-06-10 09:24:08 +0200

    la in kommentar exemplet:D (klicka på comments under en blog post så visas det)

--- frontend/views/site/blog.php
+++ frontend/views/site/blog.php
@@ -17,7 +17,12 @@ $this->params['breadcrumbs'][] = $this->title;
 					<div class="blog_list pull-left">
 						<ul class="list-unstyled">
 							<li><i class="fa fa-calendar-o"></i><span>June 3, 2013</span></li>
-							<li><a href="#"><i class="fa fa-comment"></i><span>Comments</span></a></li>
+							<li>
+								<a href="#comments-10">
+									<i class="fa fa-comment"></i>
+									<span>Comments</span>
+								</a>
+							</li>
 							<li><i class="fa fa-user"></i><span>Admin</span></li>
 							<li><a href="#"><i class="fa fa-eye"></i><span>124 views</span></a></li>
 						</ul>
@@ -37,7 +42,12 @@ $this->params['breadcrumbs'][] = $this->title;
 					<div class="blog_list pull-left">
 						<ul class="list-unstyled">
 							<li><i class="fa fa-calendar-o"></i><span>June 3, 2013</span></li>
-							<li><a href="#"><i class="fa fa-comment"></i><span>Comments</span></a></li>
+							<li>
+								<a href="#comments-10">
+									<i class="fa fa-comment"></i>
+									<span>Comments</span>
+								</a>
+							</li>
 							<li><i class="fa fa-user"></i><span>Admin</span></li>
 							<li><a href="#"><i class="fa fa-eye"></i><span>124 views</span></a></li>
 						</ul>
@@ -97,3 +107,23 @@ $this->params['breadcrumbs'][] = $this->title;
 			<div class="clearfix"></div>
 		</div>
 </div><!-- end main -->
+<div id="comments" class="col-xs-offset-1 col-xs-11 col-sm-6 col-md-4">
+	<h1 class="title">
+		Comments
+		<button type="button" class="close" aria-hidden="true">×</button>
+	</h1>
+	<div id="disqus_thread"></div>
+	<script type="text/javascript">
+		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+		var disqus_shortname = 'bootsnipp'; // required: replace example with your forum shortname
+		var disqus_identifier = '4l0k2';
+		/* * * DON'T EDIT BELOW THIS LINE * * */
+		(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
+	</script>
+	<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+	<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+</div>
--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -13,3 +13,44 @@ $(document).ready(function(){
     /** end site/index.php**/
 });
 /**END DOCUMENT READY SECTIONEN**/
+/** händelsehanterare **/
+$(function() {
+    /*
+    *Exempel: scrollar vyn till elementId vid klick på länkar som har href="#elementId"
+    * */
+    $('a[href*=#]:not([href=#])').on("click",function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    /**
+     * Comments
+     */
+    $('a[href^="#comments-"]').on('click', function(event) {
+        event.preventDefault();
+
+        // THIS IS WHERE AJAX CODE WOULD GO TO LOAD ARTICLES DYNAMICALLY
+        $('#comments').toggleClass('active');
+    });
+
+    $('article > .title > .close').on('click', function(event) {
+        event.preventDefault();
+        $('#comments').removeClass('active');
+        $(this).closest('article').removeClass('active');
+    });
+    $('#comments > .title > .close').on('click', function(event) {
+        event.preventDefault();
+        $(this).closest('#comments').removeClass('active');
+    });
+    /**
+     * End comments
+     */
+});
