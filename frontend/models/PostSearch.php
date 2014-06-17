b93d5939e69a6d47c332efa0b77c14cc826f56c0
Daniel van den Berg <idrini@gmail.com>
2014-06-17 14:14:16 +0200

    post-crud

---
+++ frontend/models/PostSearch.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Post;
+
+/**
+ * PostSearch represents the model behind the search form about `app\models\Post`.
+ */
+class PostSearch extends Post
+{
+    public function rules()
+    {
+        return [
+            [['id', 'status', 'create_time', 'update_time', 'author_id'], 'integer'],
+            [['title', 'content'], 'safe'],
+        ];
+    }
+
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    public function search($params)
+    {
+        $query = Post::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'status' => $this->status,
+            'create_time' => $this->create_time,
+            'update_time' => $this->update_time,
+            'author_id' => $this->author_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'content', $this->content]);
+
+        return $dataProvider;
+    }
+}
---
+++ frontend/views/site/index - Copy.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @var yii\web\View $this
+ */
+$this->title = 'Personal';
+?>
+<div class="site-index">
+	<div class="body-content">
+		<div class="main_bg"  id="about"><!-- start about us -->
+			<div class="container sideBorder">
+				<div class="">
+					<div class="carousel" id="quote-carousel" style="margin-left: auto;margin-right: auto; max-width: 100%;">
+						<div class="slide">
+							<blockquote>
+								<div class="row">
+									<div class="col-sm-3 text-center">
+										<img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_1.png" style="width: 100px;height:100px;">
+									</div>
+									<div class="col-sm-9">
+										<p>äpplen ramlar inte långt från BUSKAGET</p>
+										<small>Gusten</small>
+									</div>
+								</div>
+							</blockquote>
+						</div>
+						<div class="slide">
+							<blockquote>
+								<div class="row">
+									<div class="col-sm-3 text-center">
+										<img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_2.png" style="width: 100px;height:100px;">
+									</div>
+									<div class="col-sm-9">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
+										<small>Someone famous</small>
+									</div>
+								</div>
+							</blockquote>
+						</div>
+<!--	                        <a class="controls left"><i class="icon-arrow-left-3"></i></a>-->
+<!--	                        <a class="controls right"><i class="icon-arrow-right-3"></i></a>-->
+					</div>
+					</p>
+				</div>
+				<div class="row about">
+					<div class="col-md-2 about_img">
+						<img src="/images/user.png" alt="" class="responsive"/>
+					</div>
+					<div class="col-md-9 about_text">
+						<h1>derp derpson</h1>
+						<h4>programmerare & utvecklare</h4>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+						<div class="soc_icons navbar-right">
+							<ul class="list-unstyled text-center">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="notice marker-on-bottom no-desktop padding10 bg-blue text-center ">
+					<p class="subheader-secondary fg-white no-margin">Work</p>
+				</div>
+				<ol class="styled">
+					<li style="width: 90%">Create page with <strong>HTML5 DOCTYPE</strong></li>
+					<li style="width: 90%">Include <strong>metro-bootstrap.css</strong></li>
+					<li style="width: 90%">Include <strong>metro.min.js</strong> (jquery.js required)</li>
+				</ol>
+			</div>
+			<div class="col-lg-4">
+				<div class="notice marker-on-bottom no-desktop padding10 bg-darkblue text-center ">
+					<p class="subheader-secondary fg-white no-margin">Education</p>
+				</div>
+				<ol class="styled">
+					<li style="width: 90%">Create page with <strong>HTML5 DOCTYPE</strong></li>
+					<li style="width: 90%">Include <strong>metro-bootstrap.css</strong></li>
+					<li style="width: 90%">Include <strong>metro.min.js</strong> (jquery.js required)</li>
+				</ol>
+			</div>
+			<div class="col-lg-4">
+				<div class="notice marker-on-bottom no-desktop padding10 bg-cobalt text-center ">
+					<p class="subheader-secondary fg-white no-margin">Spare time</p>
+				</div>
+				<ol class="styled">
+					<li style="width: 90%">Create page with <strong>HTML5 DOCTYPE</strong></li>
+					<li style="width: 90%">Include <strong>metro-bootstrap.css</strong></li>
+					<li style="width: 90%">Include <strong>metro.min.js</strong> (jquery.js required)</li>
+				</ol>
+			</div>
+		</div>
+
+    </div>
+</div>
