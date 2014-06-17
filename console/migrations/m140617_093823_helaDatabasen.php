5e8835b15419f1a57007806d8dc1c63ba153792c
Daniel van den Berg <idrini@gmail.com>
2014-06-17 11:42:35 +0200

    databasmigration fil

---
+++ console/migrations/m140617_093823_helaDatabasen.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Schema;
+
+class m140617_093823_helaDatabasen extends \yii\db\Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m140617_093823_helaDatabasen cannot be reverted.\n";
+
+        return false;
+    }
+}
