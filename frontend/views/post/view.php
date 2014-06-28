1e656c563dbb04e36cd6c0582b44db8d5bfb26a2
Daniel van den Berg <idrini@gmail.com>
2014-06-28 15:40:34 +0200

    sofies fina css på fina rubriker

--- frontend/views/post/view.php
+++ frontend/views/post/view.php
@@ -111,126 +111,105 @@ $this->params['breadcrumbs'][] = $this->title;
 	<div id="comments" class="col-xs-offset-1 col-xs-11 col-sm-6 col-md-4">
 		<h1 class="title">
 			Comments
-			<button type="button" class="close" aria-hidden="true">×</button>
+			<button id="closeComments" type="button" class="close" aria-hidden="true">×</button>
 		</h1>
-		<div id="disqus_thread"></div>
-		<script type="text/javascript">
-			/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-			var disqus_shortname = 'bootsnipp'; // required: replace example with your forum shortname
-			var disqus_identifier = '4l0k2';
-			/* * * DON'T EDIT BELOW THIS LINE * * */
-			(function() {
-				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-				dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-			})();
-		</script>
-		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-		<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-	</div>
-	<!-- SLUT DISQUS KOMMENTARER -->
-
-
-	<div class="container12345" style="display:none" id="kommentarer">
-		<div class="row">
-			<div class="panel panel-info widget">
-				<div class="panel-heading">
-					<span class="glyphicon glyphicon-comment"></span>
-					<h3 class="panel-title">
-						Coments</h3>
+		<div class="panel panel-info widget">
+			<div class="panel-heading">
+				<span class="glyphicon glyphicon-comment"></span>
+				<h3 class="panel-title">
+					Coments</h3>
 					<span class="label label-info">
 						78</span>
-				</div>
-				<div class="panel-body">
-					<ul class="list-group">
-						<li class="list-group-item">
-							<div class="row">
-								<div class="col-xs-2 col-md-1">
-									<img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
-								<div class="col-xs-10 col-md-11">
-									<div>
-										<a href="http://www.jquery2dotnet.com/2013/10/google-style-login-page-desing-usign.html">
-											Är jag breakad igen...</a>
-										<div class="mic-info">
-											By: <a href="#">sökfunktionen</a> on 2 Aug 2013
-										</div>
-									</div>
-									<div class="comment-text">
-										pls fix me
-									</div>
-									<div class="action">
-										<button type="button" class="btn btn-primary btn-xs" title="Edit">
-											<span class="glyphicon glyphicon-pencil"></span>
-										</button>
-										<button type="button" class="btn btn-success btn-xs" title="Approved">
-											<span class="glyphicon glyphicon-ok"></span>
-										</button>
-										<button type="button" class="btn btn-danger btn-xs" title="Delete">
-											<span class="glyphicon glyphicon-trash"></span>
-										</button>
+			</div>
+			<div class="panel-body">
+				<ul class="list-group">
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-xs-2 col-md-1">
+								<img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
+							<div class="col-xs-10 col-md-11">
+								<div>
+									<a href="http://www.jquery2dotnet.com/2013/10/google-style-login-page-desing-usign.html">
+										Är jag breakad igen...</a>
+									<div class="mic-info">
+										By: <a href="#">sökfunktionen</a> on 2 Aug 2013
 									</div>
 								</div>
-							</div>
-						</li>
-						<li class="list-group-item">
-							<div class="row">
-								<div class="col-xs-2 col-md-1">
-									<img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
-								<div class="col-xs-10 col-md-11">
-									<div>
-										<a href="http://bootsnipp.com/BhaumikPatel/snippets/Obgj">Such pretty</a>
-										<div class="mic-info">
-											By: <a href="#">Derpina</a> on 11 Nov 2013
-										</div>
-									</div>
-									<div class="comment-text">
-										very better than disqus! so much design! wow!
-									</div>
-									<div class="action">
-										<button type="button" class="btn btn-primary btn-xs" title="Edit">
-											<span class="glyphicon glyphicon-pencil"></span>
-										</button>
-										<button type="button" class="btn btn-success btn-xs" title="Approved">
-											<span class="glyphicon glyphicon-ok"></span>
-										</button>
-										<button type="button" class="btn btn-danger btn-xs" title="Delete">
-											<span class="glyphicon glyphicon-trash"></span>
-										</button>
-									</div>
+								<div class="comment-text">
+									pls fix me
+								</div>
+								<div class="action">
+									<button type="button" class="btn btn-primary btn-xs" title="Edit">
+										<span class="glyphicon glyphicon-pencil"></span>
+									</button>
+									<button type="button" class="btn btn-success btn-xs" title="Approved">
+										<span class="glyphicon glyphicon-ok"></span>
+									</button>
+									<button type="button" class="btn btn-danger btn-xs" title="Delete">
+										<span class="glyphicon glyphicon-trash"></span>
+									</button>
 								</div>
 							</div>
-						</li>
-						<li class="list-group-item">
-							<div class="row">
-								<div class="col-xs-2 col-md-1">
-									<img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
-								<div class="col-xs-10 col-md-11">
-									<div>
-										<a href="http://bootsnipp.com/BhaumikPatel/snippets/4ldn">Men seriöööst</a>
-										<div class="mic-info">
-											By: <a href="#">Daniel</a> on 11 Nov 2013
-										</div>
-									</div>
-									<div class="comment-text">
-										Coments? come on!
-									</div>
-									<div class="action">
-										<button type="button" class="btn btn-primary btn-xs" title="Edit">
-											<span class="glyphicon glyphicon-pencil"></span>
-										</button>
-										<button type="button" class="btn btn-success btn-xs" title="Approved">
-											<span class="glyphicon glyphicon-ok"></span>
-										</button>
-										<button type="button" class="btn btn-danger btn-xs" title="Delete">
-											<span class="glyphicon glyphicon-trash"></span>
-										</button>
+						</div>
+					</li>
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-xs-2 col-md-1">
+								<img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
+							<div class="col-xs-10 col-md-11">
+								<div>
+									<a href="http://bootsnipp.com/BhaumikPatel/snippets/Obgj">Such pretty</a>
+									<div class="mic-info">
+										By: <a href="#">Derpina</a> on 11 Nov 2013
 									</div>
 								</div>
+								<div class="comment-text">
+									very better than disqus! so much design! wow!
+								</div>
+								<div class="action">
+									<button type="button" class="btn btn-primary btn-xs" title="Edit">
+										<span class="glyphicon glyphicon-pencil"></span>
+									</button>
+									<button type="button" class="btn btn-success btn-xs" title="Approved">
+										<span class="glyphicon glyphicon-ok"></span>
+									</button>
+									<button type="button" class="btn btn-danger btn-xs" title="Delete">
+										<span class="glyphicon glyphicon-trash"></span>
+									</button>
+								</div>
 							</div>
-						</li>
-					</ul>
-					<a href="#" class="btn btn-primary btn-sm btn-block" role="button"><span class="glyphicon glyphicon-refresh"></span> More</a>
-				</div>
+						</div>
+					</li>
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-xs-2 col-md-1">
+								<img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
+							<div class="col-xs-10 col-md-11">
+								<div>
+									<a href="http://bootsnipp.com/BhaumikPatel/snippets/4ldn">Men seriöööst</a>
+									<div class="mic-info">
+										By: <a href="#">Daniel</a> on 11 Nov 2013
+									</div>
+								</div>
+								<div class="comment-text">
+									Coments? come on!
+								</div>
+								<div class="action">
+									<button type="button" class="button primary  btn-xs" title="Edit">
+										<span class="icon-pencil"></span>
+									</button>
+									<button type="button" class="button success btn-xs" title="Approved">
+										<span class="icon-checkmark"></span>
+									</button>
+									<button type="button" class="button danger btn-xs" title="Delete">
+										<span class=" icon-cancel-2"></span>
+									</button>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<a href="#" class="btn btn-primary btn-sm btn-block" role="button"><span class="glyphicon glyphicon-refresh"></span> More</a>
 			</div>
 		</div>
 	</div>
